f7e7de1213296ed5459b36942e3210419d56bed6
kevwargo <jarasz.kevwargo@gmail.com>
2014-12-19 18:09:21 +0300

    Last changes

--- experiment.py
+++ experiment.py
@@ -1,9 +1,15 @@
 #!/usr/bin/python2
 
-import sys
+import sys, os
 import cv2
 # from SimpleCV import *
 import numpy as np
+
+Templates = {}
+
+def loadTemplates(path):
+    for f in os.listdir(os.path.join(path, 'templates')):
+        Templates[os.path.splitext(f)[0]] = parseFile(os.path.join(path, os.path.join('templates', f)))
 
 def hsv2rgb(h, s, v):
     # 100-110-010-011-010-101-100
@@ -52,8 +58,34 @@ def drawPoint(img, x, y, color):
     y = int(round(y))
     cv2.line(img, (x, y), (x, y), color)
 
-# def findCircles(contours, minSize,):
+def distance(p1, p2):
+    return np.sqrt((p1[0] - p2[0])**2 + (p1[1] - p2[1])**2)
     
+def findCircles(contours, minSize, maxSize):
+    circles = [cv2.minEnclosingCircle(cv2.convexHull(cnt[:, 0, :], returnPoints=True)) for cnt in contours]
+    for c in circles[:]:
+        if c[1] < minSize or c[1] > maxSize:
+            circles.remove(c)
+        for o in circles[:]:
+            if o[1] > c[1] and distance(c[0], o[0]) < (c[1] + o[1]):
+                try: circles.remove(c)
+                except: pass
+    return circles
+
+def matchCoin(img):
+    for tname, timg in Templates.items():
+        orb = cv2.ORB()
+        kpimg, desimg = orb.detectAndCompute(img,None)
+        kptmpl, destmpl = orb.detectAndCompute(timg,None)
+        bf = cv2.BFMatcher()
+        # print(desimg, destmpl, timg)
+        matches = bf.knnMatch(desimg, destmpl, k=2)
+        good = []
+        for m,n in matches:
+            if m.distance < 0.75*n.distance:
+                good.append([m])
+        print(tname, len(good))
+    # return len(good)
 
 def processImage(src):
     color = src
@@ -62,33 +94,23 @@ def processImage(src):
     # dilated = cv2.dilate(edges, cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(5,5)))
     img = cv2.morphologyEx(edges, cv2.MORPH_CLOSE,
                            cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(13, 13)))
-    edges = np.copy(img)
+    # edges = np.copy(img)
     contours, hierarchy = cv2.findContours(img, cv2.RETR_EXTERNAL,
                                            cv2.CHAIN_APPROX_NONE)
     # empty = np.zeros(gray.shape, dtype=np.uint8)
     # cv2.circle(empty, (len(gray[0]) / 2, len(gray) / 2), len(gray[0]) / 15, 255)
     # circleSample = cv2.findContours(empty, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)[0][0]
-    # drawing = np.zeros(gray.shape + (3,))
-    for cnt in contours:
-        # if cv2.arcLength(cnt, False) > 200:
-        #     pointcount = float(len(cnt))
-        #     # cv2.drawContours(drawing, [cnt], 0,
-        #     #                   # (np.random.randint(0, 256),
-        #     #                   #  np.random.randint(0, 256),
-        #     #                   #  np.random.randint(0, 256)),
-        #     #                   255,
-        #     #                   1)
-        #     for i, vert in enumerate(cnt):
-        #         drawPoint(drawing, vert[0][0], vert[0][1],
-        #                   hsv2rgb(*generateGradient(i / pointcount, (0.2,1,1), (1,1,1))))
-        hull = cv2.convexHull(cnt[:, 0, :], returnPoints=True)
-        coin = cv2.minEnclosingCircle(hull)
-        # cv2.circle(color, tuple(map(int, coin[0])), int(coin[1]), (0, 255, 0), 3)
-        cv2.circle(edges, tuple(map(int, coin[0])), int(coin[1]), 255, 3)
-        # cv2.drawContours(drawing, [hull], 0, (255, 255, 255))
+    drawing = np.zeros(gray.shape + (3,))
+    for (x, y), r in findCircles(contours, len(img) / 30, len(img) / 5):
+        x = int(x)
+        y = int(y)
+        r = int(r)
+        matchCoin(color[y-r:y+r, x-r:x+r])
+        # drawing = color[y-r:y+r, x-r:x+r]
+        # cv2.circle(color, tuple(map(int, coin[0])), int(coin[1]), (0, 255, 255), 3)
     # cv2.HoughCircles(drawing, )
     # print([cv2.arcLength(cnt, False) for cnt in contours])
-    return edges
+    return drawing
 
 def parseFile(filename):
     # img = cv2.imread(filename, cv2.IMREAD_GRAYSCALE)
@@ -120,4 +142,5 @@ def main(args):
 
 if __name__ == '__main__':
     if len(sys.argv) > 1:
+        loadTemplates(os.path.realpath(os.path.dirname(sys.argv[0])))
         main(sys.argv[1:])
